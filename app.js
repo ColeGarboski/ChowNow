423d18a44cd90170240fd256e6a9214d58cd029b
Cole Garboski <cole.garboski@snhu.edu>
2022-10-25 19:32:43 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var vueAppOne = new Vue({
+    el: '#vue-app-one',
+    data: {
+        name:'',
+        foodType:'',
+        hasLocation: false,
+        x: 0.0000000,
+        y: 0.0000000,
+
+    },
+    methods: {
+        ShowPosition: function(x, y) {
+            this.x = x;
+            this.y = y;
+            console.log(x);
+            console.log(y);
+            this.hasLocation = true;
+        },
+        GetUserLocation: function(){
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.ShowPosition(position.coords.latitude, position.coords.longitude)
+            }
+            )
+        },
+        LogUserData: function() {
+            this.GetUserLocation();
+            console.log(this.foodType);
+        }
+        
+    },
+    computed: {
+
+    },
+});
+
