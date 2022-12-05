419bd75f1ac8aa1eb0474dd59176f6fc810ca506
ZachVandecar <99353525+ZachVandecar@users.noreply.github.com>
2022-12-05 11:44:34 -0500

    db connect

---
+++ server/helper.js
@@ -0,0 +1,18 @@
+function getOffset(currentPage = 1, listPerPage) {
+    return (currentPage - 1) * [listPerPage];
+  }
+  
+  function emptyOrRows(rows) {
+    if (!rows) {
+      return [];
+    }
+    return rows;
+  }
+  
+  module.exports = {
+    getOffset,
+    emptyOrRows
+  }
+
+
+  //this wil change
